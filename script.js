7a6a58c70083f4dc5ddd9a4fe75fb34879321309
codewithfaizan <faizanoic.com@gmail.com>
2023-11-08 23:48:35 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,25 @@
+const array = [1, 6, 8, 10, "hey", 50, "javascript"];
+
+// let i = 0;
+// while(i < array.length){
+//     console.log(array[i])
+//     i++;
+// }
+
+// for(let i =0; i <array.length; i++){
+//     console.log(array[i])
+// }
+// array.map(ele => {
+//     return console.log(ele)
+//     console.log("hi")
+// });
+
+
+array.forEach(element => {
+    console.log(element)
+    console.log(":hi")
+});
+
+
+
+
